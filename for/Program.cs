eca1774eff66f30d9c710c92115129e8d3554a04
Bestoloc <hinz8amq17x7@mail.ru>
2024-01-13 10:40:23 +0300

    Добавьте файлы проекта.

---
+++ for/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S = 1;
+            for (double i = 1; i <= 20; i++)
+            {
+                double p = 1;
+                for (double j = 1; j <= 10; j++)
+                {
+                    p += Sin(Pow(i, 3) + Pow(j, 4));
+                }
+                S += p;
+            }
+            Console.WriteLine(S);
+        }
+    }
+}
+}
